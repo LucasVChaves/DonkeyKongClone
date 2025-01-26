d8c5f0f2d55b45afb4fad41311194c795c8abdb2
Lucas <lucasvchaves@gmail.com>
2025-01-26 18:04:03 -0300

    feat: adiciona escadas e logica de escalar as escadas

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
     public float moveSpeed = 4.2f;
+    public float climbSpeed = 0.6f;
     public float jumpForce = 6f;
     public Transform groundCheckTransform;
     public float groundCheckRadius = .2f;
@@ -10,6 +11,9 @@ public class PlayerMovement : MonoBehaviour {
 
     private Rigidbody2D rb;
     private bool isGrounded;
+    private bool isNearLadder = false;
+    private bool isClimbing = false;
+    private GameObject currLadder;
     private float moveInput;
 
     private void Start() {
@@ -21,11 +25,53 @@ public class PlayerMovement : MonoBehaviour {
         if ((Input.GetButtonDown("Jump")) && isGrounded) {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
+
+        if (isNearLadder && (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))) {
+            isClimbing = true;
+            Debug.Log("Near Ladder");
+            StartClimbing();
+        }
+        if (isClimbing && (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))) {
+            isClimbing = false;
+            Debug.Log("Off Ladder");
+            StopClimbing();
+        }
     }
 
+    private void StartClimbing() {
+        rb.gravityScale = 0;
+        transform.position = new Vector2(currLadder.transform.position.x, transform.position.y);
+        Debug.Log("Started Climbing");
+    }
+
+    private void StopClimbing() {
+        rb.gravityScale = 1;
+        Debug.Log("Stop Climbing");
+    }
     private void FixedUpdate() {
         moveInput = Input.GetAxis("Horizontal");
         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+
+        if (isClimbing) {
+            float verticalInput = Input.GetAxis("Vertical");
+            transform.Translate(Vector2.up * verticalInput * climbSpeed);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Ladder")) {
+            isNearLadder = true;
+            currLadder = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.CompareTag("Ladder")) {
+            isNearLadder = false;
+            currLadder = null;
+            isClimbing = false;
+            StopClimbing();
+        }
     }
 
     // Desenha um circulo no groundcheck, para debug
